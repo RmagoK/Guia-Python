5cf4425966aa957da3f1a7e1122400a0884cd4c9
Rosheed Mago <rmagokassie@gmail.com>
2025-01-05 02:27:13 +0000

    agregando carpeta semi avanzado, area de modulo, y practicas de como funciona

---
+++ Guia Python Level Semi Avanzado.py/Modulo_saludar.py
@@ -0,0 +1,6 @@
+def saludar(name):
+    return(f"hola, {name}, mi amorcito, estas toda bella el dia de hoy")
+
+def saludar_raro(names):
+    return(f"hola,{names}, todo bien perro, que tranzas en la wea")
+    
